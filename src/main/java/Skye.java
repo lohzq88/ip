2f976d271761094c5567010d70dfbca071ba88f0
Zhi Qin <loh.zq.acc1@gmail.com>
2025-09-02 12:47:52 +0800

    Rename the new bot name

---
+++ src/main/java/Skye.java
@@ -0,0 +1,11 @@
+public class Skye {
+    public static void main(String[] args) {
+        String logo = " ____  _               \n"
+                + "/ ___|| | ___   _  ___ \n"
+                + "\\___ \\| |/ / | | |/ _ \\\n"
+                + " ___) |   <| |_| |  __/\n"
+                + "|____/|_|\\_\\\\__, |\\___|\n"
+                + "            |___/      \n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
